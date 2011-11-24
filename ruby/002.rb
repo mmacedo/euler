c2281bad552762c96f52301057f012175b87b8cd
Michel Pavan Macedo <michelpm@gmail.com>
2011-11-24 12:08:18 -0200

    Added 2 with Ruby

---
+++ ruby/002.rb
@@ -0,0 +1,17 @@
+#!/bin/env ruby
+
+# http://projecteuler.net/problem=2
+
+require './units.rb'
+
+result = 0
+last = 0
+current = 1
+while current <= 4.millions do
+  result = result + current if current.even?
+  temp = last
+  last = current
+  current = current + temp
+end
+
+puts result
---
+++ ruby/units.rb
@@ -0,0 +1,11 @@
+#!/bin/env ruby
+
+class Numeric
+  def thousands
+    self * 1000
+  end
+
+  def millions
+    self.thousands.thousands
+  end
+end
