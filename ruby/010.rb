ed9455cd13f45ebd6d9f3069a6ef64ebb36b521d
Michel Pavan Macedo <michelpm@gmail.com>
2011-11-25 21:46:12 -0200

    Added 10 with Ruby

---
+++ ruby/010.rb
@@ -0,0 +1,18 @@
+#!/ruby/env ruby
+
+# http://projecteuler.net/problem=3
+
+require './prime.rb'
+require './units.rb'
+
+sum = 0
+prime_until(2) do |prime|
+  if prime >= 2.millions
+    true
+  else
+    sum += prime
+    false
+  end
+end
+
+puts sum
