ceccdbcb121d54f388a3fbc115e6d6d836dac951
Michel Pavan Macedo <michelpm@gmail.com>
2011-11-25 21:34:02 -0200

    Added 9 with Ruby

---
+++ ruby/009.rb
@@ -0,0 +1,19 @@
+#!/ruby/env ruby
+
+# http://projecteuler.net/problem=9
+
+sum = 0
+a = 0
+begin
+  a += 1
+
+  b = 0
+  begin
+    b += 1
+
+    c = Math.sqrt(a*a + b*b)
+    sum = a + b + c
+  end while sum < 1000
+end until sum == 1000
+
+puts a * b * Integer(c)
