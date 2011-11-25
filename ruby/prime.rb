106339a6499d1e941c4a608b094f7b19bb15a8b6
Michel Pavan Macedo <michelpm@gmail.com>
2011-11-25 03:06:58 -0200

    Extracted routine to check for prime numbers with Ruby

--- ruby/prime.rb
+++ ruby/prime.rb
@@ -1,5 +1,23 @@
 #!/ruby/env ruby
 
+def prime?(n)
+  if n == 1
+    true
+  elsif n == 2
+    true
+  elsif n == 3
+    true
+  elsif n % 2 == 0
+    false
+  else
+    factor = 3
+    while factor < (n / factor) && n % factor > 0
+      factor = factor + 2
+    end
+    n % factor > 0
+  end
+end
+
 def prime_until(from = 1)
   if from <= 1 and yield 1
     1
@@ -10,13 +28,7 @@ def prime_until(from = 1)
     while from > n or ! yield n
       begin
         n = n + 2
-
-        factor = 3
-        while factor <= (n / 3) and n % factor > 0
-          factor = factor + 2
-        end
-        is_prime = n % factor > 0
-      end until is_prime
+      end until prime? n
     end
     n
   end
