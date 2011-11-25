fba842e631d584ca0108f40a3e49c3bacc7436fd
Michel Pavan Macedo <michelpm@gmail.com>
2011-11-25 18:30:22 -0200

    Added 6 with Ruby

---
+++ ruby/006.rb
@@ -0,0 +1,13 @@
+#!/ruby/env ruby
+
+# http://projecteuler.net/problem=6
+
+def sum_of_squares(max)
+  (1..max).collect {|n| n * n }.inject :+
+end
+
+def square_of_sums(max)
+  (1..max).inject(:+) ** 2
+end
+
+puts square_of_sums(100) - sum_of_squares(100)
